37f1cd273fd09a100bf9b2353ac4d486727de5e2
Filipe Andrade <filipeandrade.work@gmail.com>
2024-08-23 23:41:20 -0300

    adicionando banco de dados (ainda não funciona corretamente)

---
+++ src/Mercadinho.Prateleira.Infrastructure.Data/DataMappings/EstoqueMapping.cs
@@ -0,0 +1,32 @@
+using Mercadinho.Prateleira.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mercadinho.Prateleira.Infrastructure.Data.DataMappings
+{
+    public class EstoqueMapping : IEntityTypeConfiguration<Estoque>
+    {
+        public void Configure(EntityTypeBuilder<Estoque> builder)
+        {
+            builder.ToTable("ESTOQUE")
+                .OwnsOne(n => n.InfoCompra)
+                .Property(p=> p.PrecoUnitario)
+                .HasColumnName("PRECO_UNITARIO")
+                .HasPrecision(16,4);
+
+            builder.OwnsOne(n => n.InfoCompra)
+                .Property(p => p.Quantidade)
+                .HasColumnName("QUANTIDADE");
+
+            builder.OwnsOne(n => n.InfoCompra)
+                .Property(p => p.UnidadeMedida)
+                .HasColumnName("UNIDADE_MEDIDA");
+
+            builder.HasOne(n => n.Produto)
+                .WithOne(n => n.Estoque)
+                .HasForeignKey<Estoque>(k => k.ProdutoId);
+                
+            
+        }
+    }
+}
---
+++ src/Mercadinho.Prateleira.Infrastructure.Data/DataRegistration/DataRegistration.cs
@@ -0,0 +1,21 @@
+using Mercadinho.Prateleira.Infrastructure.Data.Contract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mercadinho.Prateleira.Infrastructure.Data.DataRegistration
+{
+    public static class DataRegsitration
+    {
+        public static IServiceCollection AddDataRegistration(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextPool<DbContext, PrateleiraDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("sql_connection"));
+            });
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            return services;
+        }
+    }
+}
